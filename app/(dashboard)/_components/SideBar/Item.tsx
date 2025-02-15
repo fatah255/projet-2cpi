77d64254c774a48a64f2cd6143e4d1cd606b04d7
fatah <fathlbkart@gmail.com>
2025-02-15 13:45:42 -0800

    fix bug

--- app/(dashboard)/_components/SideBar/Item.tsx
+++ app/(dashboard)/_components/SideBar/Item.tsx
@@ -1,6 +1,8 @@
 "use client";
+
 import Image from "next/image";
-import { useOrganizationList, useOrganization } from "@clerk/nextjs";
+import { useOrganization, useOrganizationList } from "@clerk/nextjs";
+
 import { cn } from "@/lib/utils";
 import { Hint } from "@/components/hint";
 
@@ -9,10 +11,18 @@ interface ItemProps {
   name: string;
   imageUrl: string;
 }
+
 export const Item = ({ id, name, imageUrl }: ItemProps) => {
   const { organization } = useOrganization();
   const { setActive } = useOrganizationList();
+
   const isActive = organization?.id === id;
+
+  const onClick = () => {
+    if (!setActive) return;
+
+    setActive({ organization: id });
+  };
 
   return (
     <div className="aspect-square relative">
@@ -21,11 +31,7 @@ export const Item = ({ id, name, imageUrl }: ItemProps) => {
           fill
           alt={name}
           src={imageUrl}
-          onClick={() => {
-            if (!isActive) return;
-
-            setActive({ organization: id });
-          }}
+          onClick={onClick}
           className={cn(
             "rounded-md cursor-pointer opacity-75 hover:opacity-100 transition",
             isActive && "opacity-100"
